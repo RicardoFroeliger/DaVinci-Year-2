2c5409c46d096b2e92e63c1d5770e805f7cade84
Ricardo Froeliger <99064304@mydavinci.nl>
2022-02-09 21:04:41 +0100

    Changed styling and seperated html into different pages

--- Blok 7 + 8/Front-end & Back-end Challenges/Front-end Challenge/js/script.js
+++ Blok 7 + 8/Front-end & Back-end Challenges/Front-end Challenge/js/script.js
@@ -1,57 +1,92 @@
-let title = document.querySelector('h1.title');
-let statement = document.querySelector('h3.statement');
+let title = document.querySelector('.title');
+let statement = document.querySelector('.statement');
+let counter = document.querySelector('.counter');
 let subjectIndex = 0, answers = [];
 
-subjects = [subjects[0], subjects[1], subjects[2]];
+// subjects = [subjects[0], subjects[1], subjects[2]];
 
-title.innerHTML = subjects[0].title;
-statement.innerHTML = subjects[0].statement;
-parties.map(p => p.match = 0);
+function updateStatement() {
+    counter.innerHTML = `${subjectIndex + 1}/${subjects.length}`;
+    title.innerHTML = subjects[subjectIndex].title;
+    statement.innerHTML = subjects[subjectIndex].statement;
+}
+updateStatement();
 
-function start() {
-    let startContainer = document.querySelector('div.startContainer').style;
-    let voteContainer = document.querySelector('div.voteContainer').style;
 
-    startContainer.display = startContainer.display == 'none' ? 'block' : 'none';
-    voteContainer.display = voteContainer.display == 'none' ? 'block' : 'none';
+function generateImportanceCheckboxes() {
+    let container = document.createElement('div');
+    container.className = 'importanceContainer';
+
+    for (let [i, subject] of subjects.entries()) {
+        let div = document.createElement('div');
+        let input = document.createElement('input');
+        let label = document.createElement('label');
+
+        input.type = 'checkbox';
+        input.id = `subject${i}`;
+
+        label.setAttribute('for', `subject${i}`);
+        label.innerHTML = ` ${subject.title}`;
+
+        div.appendChild(input);
+        div.appendChild(label);
+
+        container.appendChild(div);
+    }
+    document.body.appendChild(container);
+}
+
+function togglePartyOpinions() {
+    
 }
 
 function clickVoteButton(opinion) {
-    subjectIndex++
-    
-    answers.push({ subject: subjectIndex, opinion });
-    if (!subjects[subjectIndex]) return end();
-
-    title.innerHTML = subjects[subjectIndex].title
-    statement.innerHTML = subjects[subjectIndex].statement;
+    subjectIndex++;
+    answers.push({ subject: subjectIndex - 1, opinion });
+    if (!subjects[subjectIndex]) return validateAnswers();
+    updateStatement();
 }
-
 function clickSkipButton() {
-    subjectIndex++
-
-    if (!subjects[subjectIndex]) return end();
-
-    title.innerHTML = subjects[subjectIndex].title
-    statement.innerHTML = subjects[subjectIndex].statement;
+    subjectIndex++;
+    if (!subjects[subjectIndex]) return validateAnswers();
+    updateStatement();
 }
-
 function clickBackButton() {
-    subjectIndex--
-
-    if (!subjects[subjectIndex]) return start();
-
-    title.innerHTML = subjects[subjectIndex].title
-    statement.innerHTML = subjects[subjectIndex].statement;
+    subjectIndex--;
+    if (!subjects[subjectIndex]) return window.location = '../index.html';
+    updateStatement();
 }
 
-function end() {
-    if(answers.length < 1) console.log('not enough info');
 
-    // console.log('out')
-    statement.innerHTML = 'done'
-    document.querySelector('div.buttonContainer').style.display = 'none';
-    console.log(answers);
+
+function validateAnswers() {
+
+    window.location = './importance.html';
+    // if(answers)
+    // else 
+    // toggleImportanceScreen();
 }
+
+
+// function calculateMatchingParties() {
+//     let matchingParties = [];
+
+//     for (let answer of answers) {
+
+//     }
+//     console.log(answers)
+// }
+// function toggleEndScreen() {
+//     if (answers.length < 1) console.log('not enough info');
+
+//     // console.log('out')
+//     statement.innerHTML = 'done'
+//     document.querySelector('div.buttonContainer').style.display = 'none';
+//     calculateMatchingParties();
+//     toggleImportanceScreen();
+// }
+
+
 
 // make it so that if the opinion matches the party's opinion
 // match = +1, no match = -1, no opinion = nothing
