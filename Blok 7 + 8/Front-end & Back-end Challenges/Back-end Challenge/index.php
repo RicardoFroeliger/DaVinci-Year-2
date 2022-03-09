559d1d6a8d904aa4a5c74274b9df0fbaa9942c84
Ricardo Froeliger <99064304@mydavinci.nl>
2022-03-09 14:33:27 +0100

    Added create & delete list functionality

--- Blok 7 + 8/Front-end & Back-end Challenges/Back-end Challenge/index.php
+++ Blok 7 + 8/Front-end & Back-end Challenges/Back-end Challenge/index.php
@@ -1,12 +1,16 @@
 <?php
-    require './php/database.php';
+    require_once './php/database.php';
 
-    $lists = getLists('lists');    
+    $allLists = getLists('lists'); 
+    
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $newlistName = sanitize($_POST['newListName']);
+        $deletedListId = sanitize($_POST['deletedListId']);
+
+        if(isset($newlistName) && !empty($newlistName)) createList($newlistName);
+        if(isset($deletedListId) && !empty($deletedListId)) deleteList($deletedListId);
+    }
 ?>
-
-<!-- <pre> -->
-<!-- <? var_dump($lists);?> -->
-<!-- </pre> -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -20,7 +24,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"></script>
-
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/8254289b72.js" crossorigin="anonymous"></script>
     <title>Document</title>
     <link rel="stylesheet" href="./css/style.css">
 </head>
@@ -28,30 +33,50 @@
 <body>
     <main class="container">
         <div class="row">
-            <?php for($i = 0; $i < 6; $i++) { ?>
+            <? foreach($allLists as $list) { ?>
             <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
                 <div class="LIST">
-                    <div class="LIST_NAME">
-                        <h1>List name</h1>
+                    <div class="LIST_HEADER">
+                        <h1 title=<?=$list['name']?>><?=$list['name']?></h1>       
+                        <span class="LIST_DELETE_BUTTON">
+                            <form method="POST">
+                                <button type="submit" name="deletedListId" value=<?=$list['id']?>>
+                                    <i class="fas fa-trash-alt"></i>
+                                </button>
+                            </form>    
+                        </span>
                     </div>
 
                     <div class="LIST_TASKS">
                         <div class="TASK">
-                            <h1>st corporis fugiat quod. Reiciendis in illum praesentium consequatur nostrum. Molestias,
-                                voluptatum.</h1>
+                            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, atque? Excepturi reiciendis aliquid aspernatur sunt perferendis.</h1>
                         </div>
                         <div class="TASK">
-                            <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae beatae laborio</h1>
-                        </div>
-                        <div class="TASK">
-                            <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis ipsa magni maxime
-                                enim temporibus, officiis voluptates, est veniam iusto eum nam sunt debitis explicabo,
-                                dicta quibusdam quis! Quibusdam, alias nulla?</h1>
+                            <h1>Dicta iusto aspernatur consequuntur harum, veritatis dignissimos veniam, molestias dolor in ex aliquid eaque.</h1>
                         </div>
                     </div>
                 </div>
             </div>
             <? } ?>
+            <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                <div class="CREATE_NEW_LIST">
+                    <div class="LIST_HEADER">
+                        <h1>
+                            <i class="fas fa-plus"></i>
+                            New list
+                        </h1>
+                    </div>
+                    <form method="POST">
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="newListName" placeholder="Enter name of new list..." style="display: block"> 
+                            <input class="btn btn-primary" type="submit" value="Create" style="display: inline-block">
+                            <button class="btn btn-secondary">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </main>
 </body>
--- Blok 7 + 8/Front-end & Back-end Challenges/Back-end Challenge/php/database.php
+++ Blok 7 + 8/Front-end & Back-end Challenges/Back-end Challenge/php/database.php
@@ -1,25 +1,44 @@
 <?php
-    function executeQuery($query, $params = [], $lastId = false) {
-        $connection = new PDO('mysql:host=localhost;dbname=year_3_back-end-challenge;', 'root', 'mysql');
 
-        $data = $connection->prepare($query);
-        $data ->execute($params);
-        $data = $data->fetchAll();
+function executeQuery($query, $params = [], $lastId = false) {
+    $connection = new PDO('mysql:host=localhost;dbname=year_3_back-end-challenge;', 'root', 'mysql');
 
-        $last_id = $connection->lastInsertId();
-        $connection = null;
+    $data = $connection->prepare($query);
+    $data ->execute($params);
+    $data = $data->fetchAll();
 
-        return $lastId ? [$data, $last_id] : $data;
-    }
+    $last_id = $connection->lastInsertId();
+    $connection = null;
 
-    function sanitize($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
+    return $lastId ? [$data, $last_id] : $data;
+}
 
-    function getLists($table, $orderBy = 'id') {
-        return executeQuery("SELECT * FROM $table ORDER BY $orderBy");
-    }
+function sanitize($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+function getLists($table, $orderBy = 'id') {
+    return executeQuery("SELECT * FROM $table ORDER BY $orderBy");
+}
+
+function createList($newListName) {
+    executeQuery(
+        'INSERT INTO lists(name) VALUES(:newListName)', 
+        [':newListName' => sanitize($newListName)]
+    );
+    return header("Refresh:0");
+}
+
+function deleteList($deletedListId) {
+    // echo '<script>confirm(\'hi\')</script>';
+    executeQuery(
+        'DELETE FROM lists WHERE id=:id', 
+        [':id' => sanitize($deletedListId)]
+    );
+    return header("Refresh:0");
+}
+
 ?>
