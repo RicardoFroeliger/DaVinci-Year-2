a88dbfb0d01827d8159c3c0b179fa747e0dd569e
Ricardo Froeliger <99064304@mydavinci.nl>
2022-04-01 09:27:22 +0200

    Made battles and health for pokemon (commit 29-3-2022)

--- Blok 7 + 8/Object-Oriented Programming (OOP)/OOP Pokemon battle/Pokemon.php
+++ Blok 7 + 8/Object-Oriented Programming (OOP)/OOP Pokemon battle/Pokemon.php
@@ -1,7 +1,16 @@
 <?php
 
 class Pokemon {
-    public function __construct($name, $EnergyType, $hitpoints, $health, $Attacks, $Weakness, $Resistance) {
+    public $name;
+    protected $EnergyType;
+    protected $hitpoints;
+    public $health;
+    protected $Attacks;
+    protected $Weakness;
+    protected $Resistance;
+
+
+    protected function __construct($name, $EnergyType, $hitpoints, $health, $Attacks, $Weakness, $Resistance) {
         $this->name = $name;
         $this->EnergyType = $EnergyType;
         $this->hitpoints = $hitpoints;
@@ -11,12 +20,18 @@ class Pokemon {
         $this->Resistance = $Resistance;
     }
 
-    public function __toString() {
-        return json_encode($this);
-    }
+    public function attack($attackIndex, $enemy) {
+        $attack = $this->Attacks[$attackIndex];
 
-    public function attack() {
+        if($enemy->Weakness->EnergyType == $attack['EnergyType']->name) { // $this->EnergyType->name 
+            $attack['damage'] *= $enemy->Weakness->multiplier;
+        }
 
+        if($enemy->Resistance->EnergyType == $attack['EnergyType']->name) { // $this->EnergyType->name
+            $attack['damage'] -= $enemy->Resistance->resistance;
+        }
+
+        return $attack;
     }
 }
 
@@ -48,9 +63,13 @@ class Resistance {
 }
 
 class Pikachu extends Pokemon {
-    public function __construct($name) {
+    public $name;
+    public $hitpoints;
+    public $Attacks;
+
+    public function __construct($name, $hitpoints, $Attacks) {
+        $health = $hitpoints;
         $EnergyType = new EnergyType('Lightning');
-        $hitpoints = 60;
         $Weakness = new Weakness('Fire', 1.5);
         $Resistance = new Resistance('Fighting', 20);
         parent::__construct($name, $EnergyType, $hitpoints, $health, $Attacks, $Weakness, $Resistance);
@@ -58,11 +77,31 @@ class Pikachu extends Pokemon {
 }
 
 class Charmeleon extends Pokemon {
-    public function __construct($name) {
+    public $name;
+    public $hitpoints;
+    public $Attacks;
+    
+    public function __construct($name, $hitpoints, $Attacks) {
+        $health = $hitpoints;
         $EnergyType = new EnergyType('Fire');
-        $hitpoints = 60;
-        $Weakness = new Weakness('Fire', 1.5);
-        $Resistance = new Resistance('Fighting', 20);
+        $Weakness = new Weakness('Water', 2);
+        $Resistance = new Resistance('Lightning', 10);
         parent::__construct($name, $EnergyType, $hitpoints, $health, $Attacks, $Weakness, $Resistance);
+    }
+}
+
+class Fight {
+    public $pokemon1;
+    public $pokemon2;
+
+    public function __construct($pokemon1, $pokemon2) {
+        $this->pokemon1 = $pokemon1;
+        $this->pokemon2 = $pokemon2;
+    }
+
+    public function attack ($attackIndex) {
+        $attack = $this->pokemon1->attack($attackIndex, $this->pokemon2);
+        $this->pokemon2->health -= $attack['damage'];
+        return $attack;
     }
 }
--- Blok 7 + 8/Object-Oriented Programming (OOP)/OOP Pokemon battle/index.php
+++ Blok 7 + 8/Object-Oriented Programming (OOP)/OOP Pokemon battle/index.php
@@ -2,27 +2,43 @@
 
 require_once('Pokemon.php');
 
-$gengar = new Pokemon(
-    'Gengar', 
-    new EnergyType('Ghost'),
-    10,
-    50,
-    [
-        new Attack('Shadow Claw', 100, new EnergyType('Ghost')),
-        new Attack('Self destruct', 150, new EnergyType('Normal'))
-    ],
-    new Weakness(new EnergyType('Fire'), 2),
-    new Resistance(new EnergyType('Water'), 10)
-);
+// $gengar = new Pokemon(
+//     'Gengar', 
+//     new EnergyType('Ghost'),
+//     10,
+//     50,
+//     [
+//         new Attack('Shadow Claw', 100, new EnergyType('Ghost')),
+//         new Attack('Self destruct', 150, new EnergyType('Normal'))
+//     ],
+//     new Weakness(new EnergyType('Fire'), 2),
+//     new Resistance(new EnergyType('Water'), 10)
+// );
+// print_r('<h4>'.$pokemon.'</h4>');
 
-$pikachu = new Pikachu('Pika');
+$pikachu = new Pikachu('Pika', 60, [
+    ['name' => 'Electro Ball', 'damage' => 50, 'EnergyType' => new EnergyType('Electric')],
+    ['name' => 'Thunder Punch', 'damage' => 20, 'EnergyType' => new EnergyType('Electric')]
+]);
 
-$charmeleon = new Charmeleon('Flame boi');
+$charmeleon = new Charmeleon('Flame boi', 60, [
+    ['name' => 'Headbutt', 'damage' => 10, 'EnergyType' => new EnergyType('Normal')],
+    ['name' => 'Ember', 'damage' => 30, 'EnergyType' => new EnergyType('Fire')]
+]);
+
+echo "<p>{$pikachu->name} has {$pikachu->health} health.</p>"; 
+echo "<p>{$charmeleon->name} has {$charmeleon->health} health.</p>";
 
 
+function fight ($fight, $attackIndex) {
+    $attack = $fight->attack($attackIndex);
+    
+    echo "<p>{$fight->pokemon1->name} attacks {$fight->pokemon2->name} ".
+        "with {$attack['name']} for {$attack['damage']} damage.<p>";
+    
+    echo "<p>{$fight->pokemon2->name} has {$fight->pokemon2->health} health.</p>"; 
+}
 
-print_r('<h4>'.$pokemon.'</h4>');
+fight(new Fight($pikachu, $charmeleon), 0);
 
-print_r('<h4>'.$pikachu.'</h4>');
-
-print_r('<h4>'.$charmeleon.'</h4>');
+fight(new Fight($charmeleon, $pikachu), 1); 
