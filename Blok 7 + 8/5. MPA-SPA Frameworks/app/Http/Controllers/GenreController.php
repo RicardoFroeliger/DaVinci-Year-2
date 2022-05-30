7507f38bba85cfdf4844c69f4a459d754a46c0a1
RicardoFroeliger <RicardoFroeliger@users.noreply.github.com>
2022-05-30 14:45:23 +0200

    Made genre data display dynamically from database

--- Blok 7 + 8/5. MPA-SPA Frameworks/app/Http/Controllers/GenreController.php
+++ Blok 7 + 8/5. MPA-SPA Frameworks/app/Http/Controllers/GenreController.php
@@ -2,26 +2,31 @@
 
 namespace App\Http\Controllers;
 
-// use App\Models\Genre;
+use App\Models\Genre;
 use Illuminate\Http\Request;
 
 class GenreController extends Controller
 {
     public function index()
     {
-        $genres = [
-            ['name' => 'Pop', 'url' => 'pop'],
-            ['name' => 'House', 'url' => 'house'],
-            ['name' => 'Hiphop / Rap', 'url' => 'hiphoprap'],
-            ['name' => 'Electronic', 'url' => 'electronic'],
-            ['name' => 'Rock', 'url' => 'rock']
-        ];
+        // $genres = [
+        //     ['name' => 'Pop', 'url' => 'pop'],
+        //     ['name' => 'House', 'url' => 'house'],
+        //     ['name' => 'Hiphop / Rap', 'url' => 'hiphoprap'],
+        //     ['name' => 'Electronic', 'url' => 'electronic'],
+        //     ['name' => 'Rock', 'url' => 'rock']
+        // ];
+        $genres = Genre::all();
 
         return view('genres', ['genres' => $genres]);
     }
 
     public function show($genre)
     {
-        return view('genre', ['genre' => $genre]);
+        if ($genre) {
+            return view('genre', ['genre' => $genre]);
+        } else {
+            return view('genres');
+        }
     }
 }
--- Blok 7 + 8/5. MPA-SPA Frameworks/resources/views/genres.blade.php
+++ Blok 7 + 8/5. MPA-SPA Frameworks/resources/views/genres.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 @foreach ($genres as $genre)
-    <h1><a href="/genres/{{$genre['url']}}">{{$genre['name']}}</a></h1>
+    <h1><a href="/genres/{{$genre->id}}">{{$genre->name}}</a></h1>
 @endforeach
 
 @endsection
