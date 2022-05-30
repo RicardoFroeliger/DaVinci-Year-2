904252444a28205523a454ff3f82313b5e439141
RicardoFroeliger <RicardoFroeliger@users.noreply.github.com>
2022-05-30 14:26:30 +0200

    Made the genre view put data in dynamically

--- Blok 7 + 8/5. MPA-SPA Frameworks/resources/views/genres.blade.php
+++ Blok 7 + 8/5. MPA-SPA Frameworks/resources/views/genres.blade.php
@@ -2,10 +2,8 @@
 
 @section('content')
 
-<h1><a href="/genres/pop">Pop</a></h1>
-<h1><a href="/genres/house">House</a></h1>
-<h1><a href="/genres/hiphoprap">Hiphop / Rap</a></h1>
-<h1><a href="/genres/electronic">Electronic</a></h1>
-<h1><a href="/genres/rock">Rock</a></h1>
+@foreach ($genres as $genre)
+    <h1><a href="/genres/{{$genre['url']}}">{{$genre['name']}}</a></h1>
+@endforeach
 
 @endsection
