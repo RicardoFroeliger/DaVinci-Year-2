a155117fe3676ca8e3581504e9dc7b7b0d3a4b01
RicardoFroeliger <RicardoFroeliger@users.noreply.github.com>
2022-05-30 11:31:42 +0200

    Made genre overview on home page (commit 26-4-2022)

---
+++ Blok 7 + 8/5. MPA-SPA Frameworks/resources/views/genre.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Genre page</h1>
+<h3>Genre: {{ $genre }}</h3>
+
+@endsection
--- Blok 7 + 8/5. MPA-SPA Frameworks/routes/web.php
+++ Blok 7 + 8/5. MPA-SPA Frameworks/routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/genres/{genre}', function ($genre) {
+    return view('genre', ['genre' => $genre]);
+});
