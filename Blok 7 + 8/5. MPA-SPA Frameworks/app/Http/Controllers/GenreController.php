a2f4956bd12909b86b66b2fe96edbc45ae5be8a7
RicardoFroeliger <RicardoFroeliger@users.noreply.github.com>
2022-06-03 11:21:05 +0200

    Made default controller, migration & model for songs

--- Blok 7 + 8/5. MPA-SPA Frameworks/app/Http/Controllers/GenreController.php
+++ Blok 7 + 8/5. MPA-SPA Frameworks/app/Http/Controllers/GenreController.php
@@ -9,24 +9,13 @@ class GenreController extends Controller
 {
     public function index()
     {
-        // $genres = [
-        //     ['name' => 'Pop', 'url' => 'pop'],
-        //     ['name' => 'House', 'url' => 'house'],
-        //     ['name' => 'Hiphop / Rap', 'url' => 'hiphoprap'],
-        //     ['name' => 'Electronic', 'url' => 'electronic'],
-        //     ['name' => 'Rock', 'url' => 'rock']
-        // ];
         $genres = Genre::all();
 
         return view('genres', ['genres' => $genres]);
     }
 
-    public function show($genre)
+    public function show(Genre $genre)
     {
-        if ($genre) {
-            return view('genre', ['genre' => $genre]);
-        } else {
-            return view('genres');
-        }
+        return view('genre', ['genre' => $genre]);
     }
 }
