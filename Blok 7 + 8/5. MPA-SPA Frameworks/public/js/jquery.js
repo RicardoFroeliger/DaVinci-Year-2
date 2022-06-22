5acdbfb05438127b5e39e120fe8bcb7db41afa0f
RicardoFroeliger <RicardoFroeliger@users.noreply.github.com>
2022-06-23 00:58:08 +0200

    Made an add to/remove from playlist radio & select for songs

--- Blok 7 + 8/5. MPA-SPA Frameworks/public/js/jquery.js
+++ Blok 7 + 8/5. MPA-SPA Frameworks/public/js/jquery.js
@@ -1,52 +1,31 @@
-$(document).on('click', '#saveAsPlaylistButton', function(event) {
-    
-    event.preventDefault();
-    let href = $(this).attr('data-attr');
-    $.ajax({
-        url: href,
-        beforeSend: function() {
-            $('#loader').show();
-        },
-        // return the result
-        success: function(result) {
-            $('#saveAsPlaylistModal').modal("show");
-            // $('#saveAsPlaylistBody').html(result).show();
-        },
-        complete: function() {
-            $('#loader').hide();
-        },
-        error: function(jqXHR, testStatus, error) {
-            console.log(error);
-            alert("Page " + href + " cannot open. Error:" + error);
-            $('#loader').hide();
-        },
-        timeout: 8000
-    })
+let modals = ['saveAsPlaylist', 'managePlaylist', 'managePlaylistSongs'];
+
+modals.forEach(modal => {
+    $(document).on('click', `#${modal}Button`, function(event) {
+        event.preventDefault();
+        let href = $(this).attr('data-attr');
+        $.ajax({
+            url: href,
+            beforeSend: () => $('#loader').show(),
+            success: () => $(`#${modal}Modal`).modal('show'),
+            complete: () => $('#loader').hide(),
+            error: function(jqXHR, testStatus, error) {
+                console.log(error);
+                alert("Page " + href + " cannot open. Error:" + error);
+                $('#loader').hide();
+            },
+            timeout: 8000
+        });
+    });    
 });
 
 
-$(document).on('click', '#managePlaylistButton', function(event) {
-    
-    event.preventDefault();
-    let href = $(this).attr('data-attr');
-    $.ajax({
-        url: href,
-        beforeSend: function() {
-            $('#loader').show();
-        },
-        // return the result
-        success: function(result) {
-            $('#managePlaylistModal').modal("show");
-            // $('#managePlaylistBody').html(result).show();
-        },
-        complete: function() {
-            $('#loader').hide();
-        },
-        error: function(jqXHR, testStatus, error) {
-            console.log(error);
-            alert("Page " + href + " cannot open. Error:" + error);
-            $('#loader').hide();
-        },
-        timeout: 8000
-    })
+$('[name="songPlaylistActionRadio"]').on('change', function() {
+    let state = this.id == 'songPlaylistAdd';
+
+    $('[name="songAddPlaylistsSelect"]').toggle(state);
+    $('[name="songRemovePlaylistsSelect"]').toggle(!state);
+
+    $('#playlistAddSong').toggle(state);
+    $('#playlistRemoveSong').toggle(!state);
 });
--- Blok 7 + 8/5. MPA-SPA Frameworks/resources/views/playlist.blade.php
+++ Blok 7 + 8/5. MPA-SPA Frameworks/resources/views/playlist.blade.php
@@ -43,7 +43,7 @@
                 <div class="modal-body">
                     <div class="form-group">
                         <label for="playlistName">Playlist name</label>
-                        <input form="saveChanges" type="text" name="playlistName" class="form-control mb-3" id="playlistName"
+                        <input form="savePlaylistChanges" type="text" name="playlistName" class="form-control mb-3" id="playlistName"
                             placeholder="Playlist 1" value="{{ $playlist->name }}">
                     </div>
                 </div>
@@ -51,7 +51,7 @@
                     <form method="POST" style="margin-right: auto;" action="/playlist/{{ $playlist->id }}/destroy">
                         @csrf
                         <button type="submit" class="btn btn-danger"
-                            onclick="return confirm('Are you sure you want to delete playlist: {{ $playlist->name }}?\nThis playlist has a total duration of {{ gmdate('H:i:s', $playlist->total_duration) }}.')">
+                            onclick='return confirm(`Are you sure you want to delete playlist: "{{ $playlist->name }}"?\nThis playlist has a total duration of {{ gmdate(`H:i:s`, $playlist->total_duration) }}.`)'>
                             <h5 class="m-0">Delete playlist</h5>
                         </button>
                     </form>
@@ -60,7 +60,7 @@
                         <h5 class="m-0">Close</h5>
                     </button>
 
-                    <form method="POST" id="saveChanges" action="/playlist/{{ $playlist->id }}/update">
+                    <form method="POST" id="savePlaylistChanges" action="/playlist/{{ $playlist->id }}/update">
                         @csrf
                         <button type="submit" class="btn btn-primary">
                             <h5 class="m-0">Save changes</h5>
@@ -70,46 +70,4 @@
             </div>
         </div>
     </div>
-
-
-
-
-    {{-- <div class="modal fade" id="managePlaylistModal" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h2 class="modal-title">Manage playlist</h2>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                </div>
-                <div class="modal-body">
-                    <form>
-                        <div class="form-group">
-                            <label for="managePlaylistFormInput1">Playlist name</label>
-                            <input class="form-control mb-3" id="managePlaylistFormInput1" placeholder="Playlist 1" value="{{ $playlist->name }}">
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <form method="POST" style="margin-right: auto;" action="/playlist/{{ $playlist->id }}/destroy">
-                        @csrf
-                        <button type="submit" class="btn btn-danger"
-                            onclick="return confirm('Are you sure you want to delete playlist: {{ $playlist->name }}?\nThis playlist has a total duration of {{ gmdate('H:i:s', $playlist->total_duration) }}.')">
-                            <h5 class="m-0">Delete playlist</h5>
-                        </button>
-                    </form>
-
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                        <h5 class="m-0">Close</h5>
-                    </button>
-
-                    <form method="POST" action="/playlist/{{ $playlist->id }}/update">
-                        @csrf
-                        <button type="submit" class="btn btn-primary">
-                            <h5 class="m-0">Save changes</h5>
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div> --}}
 @endsection
